4dd7d84e2d397a752ba89fd92c4ea9b87611f971
rafael.chitan <rafael.chitan@icloud.com>
2023-12-28 19:14:16 +0200

    Added login

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -13,3 +13,14 @@ def about():
 @app.route('/buttons')
 def buttons():
     return render_template('buttons.html')
+
+# Route for handling the login page logic
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
